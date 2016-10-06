bcdbd159a1ebd6832ae8a0c890a34301e6cd072c
Alexey Loubyansky <olubyans@redhat.com>
2016-10-06 13:34:52 +0200

    feature-pack GAV completer based on the local maven repo

--- tool/src/main/java/org/jboss/provisioning/cli/GavCompleter.java
+++ tool/src/main/java/org/jboss/provisioning/cli/GavCompleter.java
@@ -22,7 +22,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.jboss.aesh.cl.completer.OptionCompleter;
@@ -34,75 +33,205 @@ import org.jboss.aesh.console.command.completer.CompleterInvocation;
  */
 public class GavCompleter implements OptionCompleter<CompleterInvocation> {
 
-    protected String repoHome = Util.getMavenRepositoryPath();
+    protected final Path repoHome = Paths.get(Util.getMavenRepositoryPath());
 
     @Override
     public void complete(CompleterInvocation ci) {
-        Path path = Paths.get(repoHome);
+        Path path = repoHome;
         if(!Files.isDirectory(path)) {
             return;
         }
-
-        final String currentValue = ci.getGivenCompleteValue();
-        int level = 1;
-        String prefix = null;
-        String chunk = null;
-        int groupSeparator = currentValue.indexOf(':');
-        if(groupSeparator > 0) {
-            path = path.resolve(currentValue.substring(0, groupSeparator));
-            ++level;
-            if(groupSeparator + 1 < currentValue.length()) {
-                int artifactSeparator = currentValue.indexOf(':', groupSeparator + 1);
-                if(artifactSeparator > 0) {
-                    ++level;
-                    path = path.resolve(currentValue.substring(groupSeparator + 1, artifactSeparator));
-                    if(artifactSeparator + 1 < currentValue.length()) {
-                        prefix = currentValue.substring(0, artifactSeparator + 1);
-                        chunk = currentValue.substring(artifactSeparator + 1);
-                    } else {
-                        prefix = currentValue;
-                        chunk = null;
-                    }
-                } else {
-                    prefix = currentValue.substring(0, groupSeparator + 1);
-                    chunk = currentValue.substring(groupSeparator + 1);
-                }
-            } else {
-                prefix = currentValue;
-            }
-        } else {
-            chunk = currentValue;
-        }
-
-        if(!Files.isDirectory(path)) {
-            return;
-        }
-
-        final List<String> candidates = new ArrayList<String>();
         try {
-            addCandidates(path, prefix, chunk, candidates, level);
+            doComplete(ci);
         } catch (IOException e) {
             return;
         }
+    }
+
+    private void doComplete(CompleterInvocation ci) throws IOException {
+        final List<String> candidates = new ArrayList<>();
+        final String currentValue = ci.getGivenCompleteValue();
+        final int groupSeparator = currentValue.indexOf(':');
+        if(groupSeparator > 0) {
+            Path currentDir = repoHome;
+            final String[] parts = currentValue.substring(0, groupSeparator).split("\\.");
+            for(String part : parts) {
+                currentDir = currentDir.resolve(part);
+            }
+            if(groupSeparator == currentValue.length() - 1) {
+                try(DirectoryStream<Path> stream = Files.newDirectoryStream(currentDir)) {
+                    for(Path p : stream) {
+                        if(isArtifactDir(p)) {
+                            candidates.add(currentValue + p.getFileName());
+                        }
+                    }
+                }
+            } else {
+                final int artifactSeparator = currentValue.indexOf(':', groupSeparator + 1);
+                if(artifactSeparator > 0) {
+                    currentDir = currentDir.resolve(currentValue.substring(groupSeparator + 1, artifactSeparator));
+                    if(artifactSeparator == currentValue.length() - 1) {
+                        try(DirectoryStream<Path> stream = Files.newDirectoryStream(currentDir)) {
+                            for(Path p : stream) {
+                                if(isVersionDir(p)) {
+                                    candidates.add(currentValue + p.getFileName());
+                                }
+                            }
+                        }
+                    } else {
+                        final String chunk = currentValue.substring(artifactSeparator + 1);
+                        final String prefix = currentValue.substring(0, artifactSeparator + 1);
+                        try(DirectoryStream<Path> stream = Files.newDirectoryStream(currentDir)) {
+                            for(Path p : stream) {
+                                if (isVersionDir(p)) {
+                                    final String dirName = p.getFileName().toString();
+                                    if (dirName.startsWith(chunk) && dirName.length() != chunk.length()) {
+                                        candidates.add(prefix + p.getFileName());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    final String chunk = currentValue.substring(groupSeparator + 1);
+                    if(Files.exists(currentDir.resolve(chunk))) {
+                        if(isArtifactDir(currentDir.resolve(chunk))) {
+                            candidates.add(currentValue + ":");
+                        }
+                    }
+                    final String prefix = currentValue.substring(0, groupSeparator + 1);
+                    try(DirectoryStream<Path> stream = Files.newDirectoryStream(currentDir)) {
+                        for(Path p : stream) {
+                            if (isArtifactDir(p)) {
+                                final String dirName = p.getFileName().toString();
+                                if (dirName.startsWith(chunk) && dirName.length() != chunk.length()) {
+                                    candidates.add(prefix + p.getFileName());
+                                }
+                            }
+                        }
+                    }
+                    ci.setAppendSpace(false);
+                }
+            }
+        } else {
+            completeGroup(currentValue, candidates);
+            ci.setAppendSpace(false);
+        }
+
         ci.addAllCompleterValues(candidates);
     }
 
-    private void addCandidates(Path path, String prefix, String chunk, final List<String> candidates, int level) throws IOException {
-        Path child = null;
-        try(DirectoryStream<Path> stream = Files.newDirectoryStream(path,
-                (Path p) -> Files.isDirectory(p) && (chunk == null ? true : p.getFileName().toString().startsWith(chunk)))) {
-            final Iterator<Path> iter = stream.iterator();
-            while(iter.hasNext()) {
-                child = iter.next();
-                candidates.add(
-                        prefix == null ? child.getFileName().toString() :
-                            prefix + child.getFileName().toString());
+    private void completeGroup(String currentValue, List<String> candidates) throws IOException {
+
+        Path groupDir = repoHome;
+        final String chunk;
+        final String prefix;
+        if(currentValue.isEmpty()) {
+            chunk = "";
+            prefix = "";
+        } else {
+            final String[] parts = currentValue.split("\\.");
+            if(currentValue.charAt(currentValue.length() - 1) == '.') {
+                chunk = "";
+                prefix = currentValue;
+                for(String part : parts) {
+                    groupDir = groupDir.resolve(part);
+                }
+            } else {
+                if (parts.length == 1) {
+                    chunk = parts[0];
+                    prefix = "";
+                } else {
+                    int i = 0;
+                    while (i < parts.length - 1) {
+                        groupDir = groupDir.resolve(parts[i++]);
+                    }
+                    chunk = parts[parts.length - 1];
+                    prefix = currentValue.substring(0, currentValue.length() - chunk.length());
+                }
+                trySeparators(groupDir.resolve(chunk), currentValue, candidates);
             }
         }
-        if(level < 3 && candidates.size() == 1) {
-            prefix = candidates.get(0) + ':';
-            candidates.clear();
-            addCandidates(child, prefix, null, candidates, level + 1);
+
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(groupDir)) {
+            for(Path p : stream) {
+                if (isGroupDir(p)) {
+                    final String dirName = p.getFileName().toString();
+                    if (chunk.isEmpty() || dirName.startsWith(chunk) && dirName.length() != chunk.length()) {
+                        candidates.add(prefix + dirName);
+                    }
+                }
+            }
         }
+    }
+
+    private void trySeparators(Path dir, String prefix, List<String> candidates) throws IOException {
+        if(!Files.isDirectory(dir)) {
+            return;
+        }
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            boolean group = false;
+            boolean artifact = false;
+            for(Path p : stream) {
+                if(!artifact && isArtifactDir(p)) {
+                    candidates.add(prefix + ":");
+                    if(group) {
+                        return;
+                    } else {
+                        artifact = true;
+                    }
+                } else if(!group && isGroupDir(p)) {
+                    candidates.add(prefix + ".");
+                    if(artifact) {
+                        return;
+                    } else {
+                        group = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isGroupDir(Path dir) throws IOException {
+        if(!Files.isDirectory(dir)) {
+            return false;
+        }
+        if(isVersionDir(dir) || isArtifactDir(dir)) {
+            return false;
+        }
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for(Path p : stream) {
+                return !isVersionDir(p);
+            }
+        }
+        return false;
+    }
+
+    private boolean isArtifactDir(Path dir) throws IOException {
+        if(!Files.isDirectory(dir)) {
+            return false;
+        }
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for(Path p : stream) {
+                if(isVersionDir(p)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isVersionDir(Path dir) throws IOException {
+        if(!Files.isDirectory(dir)) {
+            return false;
+        }
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for(Path p : stream) {
+                if(p.getFileName().toString().endsWith(".zip")) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
