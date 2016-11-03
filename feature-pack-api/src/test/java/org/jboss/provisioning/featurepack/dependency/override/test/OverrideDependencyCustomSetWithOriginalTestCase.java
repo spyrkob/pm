e31a6908a1624f5835b9aa540c978d4a68ec12ba
Alexey Loubyansky <olubyans@redhat.com>
2016-11-03 16:36:00 +0100

    more tests

---
+++ feature-pack-api/src/test/java/org/jboss/provisioning/featurepack/dependency/override/test/OverrideDependencyCustomSetWithOriginalTestCase.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.provisioning.featurepack.dependency.override.test;
+
+import org.jboss.provisioning.ArtifactCoords;
+import org.jboss.provisioning.descr.ProvisionedFeaturePackDescription;
+import org.jboss.provisioning.descr.ProvisionedInstallationDescription;
+import org.jboss.provisioning.descr.ProvisionedInstallationDescription.Builder;
+import org.jboss.provisioning.descr.ProvisioningDescriptionException;
+import org.jboss.provisioning.test.PmProvisionSpecTestBase;
+import org.jboss.provisioning.test.util.fs.state.DirState;
+import org.jboss.provisioning.test.util.fs.state.DirState.DirBuilder;
+import org.jboss.provisioning.test.util.repomanager.FeaturePackRepoManager;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class OverrideDependencyCustomSetWithOriginalTestCase extends PmProvisionSpecTestBase {
+
+    @Override
+    protected void setupRepo(FeaturePackRepoManager repoManager) throws ProvisioningDescriptionException {
+        repoManager.installer()
+            .newFeaturePack(ArtifactCoords.newGav("org.jboss.pm.test", "fp1", "1.0.0.Alpha-SNAPSHOT"))
+                .addDependency(ProvisionedFeaturePackDescription.forGav(ArtifactCoords.newGav("org.jboss.pm.test", "fp2", "2.0.0.Final")))
+                .addDependency(ProvisionedFeaturePackDescription.forGav(ArtifactCoords.newGav("org.jboss.pm.test", "fp3", "2.0.0.Final")))
+                .newPackage("p1", true)
+                    .writeContent("fp1/p1.txt", "p1")
+                    .getFeaturePack()
+                .getInstaller()
+            .newFeaturePack(ArtifactCoords.newGav("org.jboss.pm.test", "fp2", "2.0.0.Final"))
+                .addDependency(ProvisionedFeaturePackDescription
+                        .builder(ArtifactCoords.newGav("org.jboss.pm.test", "fp3", "2.0.0.Final"))
+                        .excludePackage("a")
+                        .excludePackage("b")
+                        .build())
+                .newPackage("p1", true)
+                    .writeContent("fp2/p1.txt", "p1")
+                    .getFeaturePack()
+                .getInstaller()
+            .newFeaturePack(ArtifactCoords.newGav("org.jboss.pm.test", "fp3", "2.0.0.Final"))
+                .newPackage("a", true)
+                    .writeContent("fp3/a.txt", "a")
+                    .getFeaturePack()
+                .newPackage("b", true)
+                    .addDependency("b1")
+                    .writeContent("fp3/b.txt", "b")
+                    .getFeaturePack()
+                .newPackage("b1")
+                    .writeContent("fp3/b1.txt", "b1")
+                    .getFeaturePack()
+                .newPackage("c", true)
+                    .addDependency("c1")
+                    .writeContent("fp3/c.txt", "c")
+                    .getFeaturePack()
+                .newPackage("c1")
+                    .writeContent("fp3/c1.txt", "c1")
+                    .getFeaturePack()
+                .getInstaller()
+            .install();
+    }
+
+    @Override
+    protected ProvisionedInstallationDescription provisionedInstallation(boolean includeDependencies)
+            throws ProvisioningDescriptionException {
+
+        final Builder builder = ProvisionedInstallationDescription.builder()
+                .addFeaturePack(
+                        ProvisionedFeaturePackDescription.forGav(ArtifactCoords.newGav("org.jboss.pm.test", "fp1", "1.0.0.Alpha-SNAPSHOT")));
+        if(includeDependencies) {
+            builder
+                .addFeaturePack(
+                        ProvisionedFeaturePackDescription.forGav(ArtifactCoords.newGav("org.jboss.pm.test", "fp2", "2.0.0.Final")))
+                .addFeaturePack(
+                        ProvisionedFeaturePackDescription.forGav(ArtifactCoords.newGav("org.jboss.pm.test", "fp3", "2.0.0.Final")));
+        }
+
+        return builder.build();
+    }
+
+    @Override
+    protected DirState provisionedHomeDir(DirBuilder builder) {
+        return builder
+                .addFile("fp1/p1.txt", "p1")
+                .addFile("fp2/p1.txt", "p1")
+                .addFile("fp3/a.txt", "a")
+                .addFile("fp3/b.txt", "b")
+                .addFile("fp3/b1.txt", "b1")
+                .addFile("fp3/c.txt", "c")
+                .addFile("fp3/c1.txt", "c1")
+                .build();
+    }
+}
