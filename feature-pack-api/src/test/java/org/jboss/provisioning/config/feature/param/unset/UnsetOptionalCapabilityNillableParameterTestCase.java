0e0576cf3f7d6475ff28c9dad6ff687009f80e3c
Alexey Loubyansky <loubyansky@gmail.com>
2017-12-20 14:05:26 +0100

    parameter unset and reset tests

---
+++ feature-pack-api/src/test/java/org/jboss/provisioning/config/feature/param/unset/UnsetOptionalCapabilityNillableParameterTestCase.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.provisioning.config.feature.param.unset;
+
+import org.jboss.provisioning.ArtifactCoords;
+import org.jboss.provisioning.Errors;
+import org.jboss.provisioning.ArtifactCoords.Gav;
+import org.jboss.provisioning.ProvisioningDescriptionException;
+import org.jboss.provisioning.ProvisioningException;
+import org.jboss.provisioning.config.FeatureConfig;
+import org.jboss.provisioning.config.FeaturePackConfig;
+import org.jboss.provisioning.config.ConfigModel;
+import org.jboss.provisioning.config.FeatureGroup;
+import org.jboss.provisioning.repomanager.FeaturePackRepositoryManager;
+import org.jboss.provisioning.spec.FeatureId;
+import org.jboss.provisioning.spec.FeatureParameterSpec;
+import org.jboss.provisioning.spec.FeatureSpec;
+import org.jboss.provisioning.state.ProvisionedState;
+import org.jboss.provisioning.test.PmInstallFeaturePackTestBase;
+
+/**
+ *
+ * @author Alexey Loubyansky
+ */
+public class UnsetOptionalCapabilityNillableParameterTestCase extends PmInstallFeaturePackTestBase {
+
+    private static final Gav FP_GAV = ArtifactCoords.newGav("org.jboss.pm.test", "fp1", "1.0.0.Final");
+
+    @Override
+    protected void setupRepo(FeaturePackRepositoryManager repoManager) throws ProvisioningDescriptionException {
+        repoManager.installer()
+        .newFeaturePack(FP_GAV)
+            .addSpec(FeatureSpec.builder("specA")
+                .providesCapability("cap.$p1", true)
+                .addParam(FeatureParameterSpec.createId("name"))
+                .addParam(FeatureParameterSpec.builder("p1").setNillable().setDefaultValue("spec").build())
+                .build())
+            .addSpec(FeatureSpec.builder("specB")
+                .requiresCapability("cap.spec")
+                .addParam(FeatureParameterSpec.createId("name"))
+                .build())
+            .addFeatureGroup(FeatureGroup.builder("group1")
+                    .addFeature(
+                            new FeatureConfig("specA")
+                            .setParam("name", "a1")
+                            .setParam("p1", "spec"))
+                    .build())
+            .addConfig(ConfigModel.builder()
+                    .addFeatureGroup(FeatureGroup.builder("group1")
+                            .includeFeature(FeatureId.create("specA", "name", "a1"), new FeatureConfig().unsetParam("p1"))
+                            .build())
+                    .addFeature(
+                            new FeatureConfig("specB").setParam("name", "b1"))
+                    .build())
+            .getInstaller()
+        .install();
+    }
+
+    @Override
+    protected FeaturePackConfig featurePackConfig() {
+        return FeaturePackConfig.forGav(FP_GAV);
+    }
+
+    @Override
+    protected String[] pmErrors() {
+        return new String[] {Errors.failedToBuildConfigSpec(null, null),
+                "No provider found for capability cap.spec required by org.jboss.pm.test:fp1:1.0.0.Final#specB:name=b1"};
+    }
+
+    @Override
+    protected ProvisionedState provisionedState() throws ProvisioningException {
+        return null;
+    }
+}
